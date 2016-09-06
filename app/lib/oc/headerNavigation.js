739513f1f337280b0361ccf2a43d4e6ca75a4ef1
Jen Rasmussen <jrasmussen@four51.com>
2016-09-06 08:41:17 -0500

    Custom Solutions

---
+++ app/lib/oc/headerNavigation.js
@@ -0,0 +1,42 @@
+angular.module('OrderCloud-HeaderNavigation', []);
+angular.module('OrderCloud-HeaderNavigation')
+    .directive('headernavigation', headernavigation)
+;
+
+function headernavigation() {
+    return {
+        restrict: 'E',
+        template: template
+    };
+
+    function template() {
+        return [
+            '<style>',
+            '</style>',
+            '<section class="header-navigation">',
+                '<div class="row">',
+                    '<div class="col-xs-12">',
+                        '<div class="col-xs-12 col-sm-3">',
+                            '&nbsp;',
+                        '</div>',
+                        '<div class="col-xs-12 col-sm-6">',
+                            '<div class="logo">',
+                                '<a href="catalog">',
+                                    //'<img class="img-responsive" ng-src="{{user.Company.LogoUrl}}" />',
+                                    '<img class="img-responsive" src="css/images/custom/logo.png" />',
+                                '</a>',
+                            '</div>',
+                        '</div>',
+                        '<div class="col-xs-12 col-sm-3 product-search">',
+                            '<productsearchinput></productsearchinput>',
+                        '</div>',
+                        //'<div class="col-xs-12 col-sm-4 col-md-3 minicart-wrapper">',
+                        //'<minicart></minicart>',
+                        //'</div>',
+                    '</div>',
+                '</div>',
+            '</div>',
+            '</section>'
+        ].join('');
+    }
+}
