10066df6510c733aa580e435e39bfa651e5628ba
Jen Rasmussen <jrasmussen@four51.com>
2016-09-06 19:53:52 -0500

    Custom Solutions - remove Category Navigation

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 var four51 = {};
 four51.app = angular.module('451order', [
     'ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
-    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter', 'OrderCloud-SpecForms', 'OrderCloud-Carousel', 'OrderCloud-CategoryModal', 'OrderCloud-CategoryNavigation',
-    'OrderCloud-CheckoutItemSummary', 'OrderCloud-HamburgerNavigation', 'OrderCloud-HeaderNavigation', 'OrderCloud-Minicart', 'OrderCloud-ProductSearchInput', 'OrderCloud-SameAsShippingCheckbox',
-    'OrderCloud-SecurityModal', 'OrderCloud-TermsModal'
+    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter', 'OrderCloud-SpecForms', 'OrderCloud-Carousel', 'OrderCloud-CategoryModal',
+    'OrderCloud-CheckoutItemSummary', 'OrderCloud-HamburgerNavigation', 'OrderCloud-HeaderNavigation', 'OrderCloud-Minicart', 'OrderCloud-ProductSearchInput',
+    'OrderCloud-SameAsShippingCheckbox', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal'
 ]);
