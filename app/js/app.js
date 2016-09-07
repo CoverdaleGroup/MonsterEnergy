0c411a6476f201a1618d61d1233da22afde11bb1
Jen Rasmussen <jrasmussen@four51.com>
2016-09-07 09:28:18 -0500

    Custom Solutions - Product List Add To Cart

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,6 @@ var four51 = {};
 four51.app = angular.module('451order', [
     'ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
     'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter', 'OrderCloud-SpecForms', 'OrderCloud-Carousel', 'OrderCloud-CategoryModal',
-    'OrderCloud-CheckoutItemSummary', 'OrderCloud-HamburgerNavigation', 'OrderCloud-HeaderNavigation', 'OrderCloud-Minicart', 'OrderCloud-ProductSearchInput',
-    'OrderCloud-SameAsShippingCheckbox', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal'
+    'OrderCloud-CheckoutItemSummary', 'OrderCloud-HamburgerNavigation', 'OrderCloud-HeaderNavigation', 'OrderCloud-Minicart', 'OrderCloud-ProductListAddToCart',
+    'OrderCloud-ProductSearchInput', 'OrderCloud-SameAsShippingCheckbox', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal'
 ]);
