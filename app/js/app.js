df1e3fe6d3e8884f366956ec8732db4239a8744c
Jen Rasmussen <jrasmussen@four51.com>
2016-09-25 13:50:06 -0500

    Design Updates - Revision 2 - add breadcrumb

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 var four51 = {};
 four51.app = angular.module('451order', [
     'ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
-    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter', 'OrderCloud-SpecForms', 'OrderCloud-Carousel', 'OrderCloud-CategoryModal',
+    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter', 'OrderCloud-Breadcrumbs', 'OrderCloud-SpecForms', 'OrderCloud-Carousel', 'OrderCloud-CategoryModal',
     'OrderCloud-CheckoutItemSummary', 'OrderCloud-HamburgerNavigation', 'OrderCloud-HeaderNavigation', 'OrderCloud-Minicart', 'OrderCloud-ProductListAddToCart',
     'OrderCloud-ProductSearchInput', 'OrderCloud-SameAsShippingCheckbox', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal'
 ]);
